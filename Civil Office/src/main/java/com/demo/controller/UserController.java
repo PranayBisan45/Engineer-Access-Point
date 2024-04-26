80f8e04e4b91f4720a5828d9d94d416526e472d5
PranayBisan45 <pranaybisan45@gmail.com>
2024-04-26 18:19:50 +0530

    updated

--- Civil Office/src/main/java/com/demo/controller/UserController.java
+++ Civil Office/src/main/java/com/demo/controller/UserController.java
@@ -5,12 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.demo.model.appointment;
 import com.demo.model.user;
 import com.demo.service.userService;
 
@@ -43,4 +45,10 @@ public class UserController {
 		}
 			return ResponseEntity.ok("Login Failed");
 	}
+	
+//	@GetMapping("/Search/{name}")
+//	public ResponseEntity <appointment> Search(@PathVariable String name) {
+//		appointment a = uservice.search(name);
+//		return null;
+//	}
 }
